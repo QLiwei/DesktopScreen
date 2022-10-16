48790488935de6cc3d0b158d5864a76054b485d1
qlw <1719569290@qq.com>
2022-10-16 17:33:39 +0800

    [NEW] 1.创建初始工程 2.添加esp_log

---
+++ main/hello_world_main.c
@@ -0,0 +1,55 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "esp_log.h"
+
+#ifdef CONFIG_IDF_TARGET_ESP32
+#define CHIP_NAME "ESP32"
+#endif
+
+#ifdef CONFIG_IDF_TARGET_ESP32S2BETA
+#define CHIP_NAME "ESP32-S2 Beta"
+#endif
+
+static const char *TAG = "MAIN APP";
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is %s chip with %d CPU cores, WiFi%s%s, ",
+            CHIP_NAME,
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    ESP_LOGE(TAG, "ESP_LOGE");
+    ESP_LOGW(TAG, "ESP_LOGW");
+    ESP_LOGI(TAG, "ESP_LOGI");
+    ESP_LOGD(TAG, "ESP_LOGD");
+    ESP_LOGV(TAG, "ESP_LOGV");
+
+    while(1){
+        printf("system run ...\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
